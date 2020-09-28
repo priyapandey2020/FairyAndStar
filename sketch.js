5acb9e669fd3218fe964b809967666d4928d3adf
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2020-09-28 11:19:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,7 +60,11 @@ function draw() {
 function keyPressed() {
 
 	if(keyCode === RIGHT_ARROW){
-       fairy.x = fairy.x + 20;
+           fairy.x = fairy.x + 20;
+	}
+	
+        if(keyCode === LEFT_ARROW){
+           fairy.x = fairy.x - 20;
 	}
 
 	if (keyCode === DOWN_ARROW) {
